a5d6c68d9d610d44e2a11b0a08001948df08581c
Kyle Gobel <kgobel@gmail.com>
2014-04-19 12:08:47 -0400

    created test room in ctr, and delete the test room in dispose

--- src/IntegrationTests/SendRoomNotification.cs
+++ src/IntegrationTests/SendRoomNotification.cs
@@ -1,25 +1,35 @@
-﻿using HipchatApiV2;
+﻿using System;
+using HipchatApiV2;
 using HipchatApiV2.Requests;
 using Xunit;
 
 namespace IntegrationTests
 {
     [Trait("SendNotification", "")]
-    public class SendRoomNotification
+    public class SendRoomNotification : IDisposable
     {
+        private readonly int _existingRoomId;
         public SendRoomNotification()
         {
             HipchatApiConfig.AuthToken = TestsConfig.AuthToken;
+            HipchatClient client = new HipchatClient();
+
+            _existingRoomId = client.CreateRoom("Test Send Message Room").Id;
         }
         [Fact(DisplayName = "Can send a room notification")]
         public void CanSendRoomNotification()
         {
             var client = new HipchatClient();
 
-            var sendMessageResult = client.SendNotification(TestsConfig.ExistingRoomId, "Test message");
+            var sendMessageResult = client.SendNotification(_existingRoomId, "Test message");
 
             Assert.True(sendMessageResult);
         }
-         
+
+        public void Dispose()
+        {
+            HipchatClient client = new HipchatClient();
+            client.DeleteRoom(_existingRoomId);
+        }
     }
 }
