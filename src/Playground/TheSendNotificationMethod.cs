5e6ffdc9514b62c267c1e5e9fc8ff8a71c344565
Kyle Gobel <kgobel@deerso.com>
2014-04-23 14:19:13 -0400

    rm test tokens

--- src/Playground/TheSendNotificationMethod.cs
+++ src/Playground/TheSendNotificationMethod.cs
@@ -19,8 +19,8 @@ namespace Playground
                 {
                     TokenScope.SendNotification,
                 },
-                "4471354c-7e43-44f5-9f44-7f1cb10acae0", /*Auth Id*/
-                "hD7C0xkK2LUoEiqTeq7xmIcDTifL3IaAZgVfz64l" /*Auth Secret*/);
+                "", /*Auth Id*/
+                "" /*Auth Secret*/);
 
             Assert.NotNull(token);
         }
