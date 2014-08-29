a97e548c64f9379b125fce5abcf86807eeadca2e
bartmcv <bart.venderbosch@gmail.com>
2014-08-29 10:50:14 +0200

    Changed source file formatting

--- src/Api/Requests/CreateUserRequest.cs
+++ src/Api/Requests/CreateUserRequest.cs
@@ -1,5 +1,7 @@
-﻿namespace HipchatApiV2.Requests {
-    public class CreateUserRequest {
+﻿namespace HipchatApiV2.Requests
+﻿{
+    public class CreateUserRequest
+    {
         public string Name { get; set; }        // User's full name. Valid length range: 1 - 50
         public string Email { get; set; }
         public string Password { get; set; }    // Optional. If not provided, a randomly generated password will be returned
