a47be08e36d88abc00dbeb16c468859de06b98dd
Kyle <kgobel@gmail.com>
2014-12-20 13:15:40 -0500

    started adding some typed exceptions
    mainly to help with inconsistant tests.
    
    Add HipchatRoomNotFoundException

---
+++ src/IntegrationTests/TestHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HipchatApiV2;
+using HipchatApiV2.Exceptions;
+
+namespace IntegrationTests
+{
+    public class TestHelpers
+    {
+        public static int GetARoomId(HipchatClient client, string roomName)
+        {
+            try
+            {
+                var room = client.GetRoom(roomName);
+                return room.Id;
+            }
+            catch (HipchatRoomNotFoundException)
+            {
+                var room = client.CreateRoom(roomName);
+                return room.Id;
+            }
+        }
+    }
+}
