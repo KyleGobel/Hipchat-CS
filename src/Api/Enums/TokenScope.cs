5adbe92ffe0337c99e0ba4c9dd95e7fe3ae2fdb9
Murali M <murali@muralim.com>
2014-12-31 15:44:47 -0600

    Corrected tokenScope enum and added a new enum.
    Corrected TokenScope enum "ManageRoom" and added new scope
    "ViewMessages".

--- src/Api/Enums/TokenScope.cs
+++ src/Api/Enums/TokenScope.cs
@@ -20,9 +20,10 @@
 
         public static readonly TokenScope AdminGroup = new TokenScope("admin_group");
         public static readonly TokenScope AdminRoom = new TokenScope("admin_room");
-        public static readonly TokenScope ManageRoom = new TokenScope("manage_room");
+        public static readonly TokenScope ManageRooms = new TokenScope("manage_rooms");
         public static readonly TokenScope SendMessage = new TokenScope("send_message");
         public static readonly TokenScope SendNotification = new TokenScope("send_notification");
         public static readonly TokenScope ViewGroup = new TokenScope("view_group");
+        public static readonly TokenScope ViewMessages = new TokenScope("view_messages");
     }
 }
