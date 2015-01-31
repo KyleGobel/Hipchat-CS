4dbce946b8c52bd70fd9bbd57f60ba1429644144
Kyle <kgobel@gmail.com>
2015-01-31 01:16:14 -0500

    added test to project

---
+++ src/IntegrationTests/SetRoomTopicTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HipchatApiV2;
+using Xunit;
+
+namespace IntegrationTests
+{
+    [Trait("Set Room Topic", "")]
+    public class SetRoomTopicTests 
+    {
+        private readonly IHipchatClient _client;
+        public SetRoomTopicTests()
+        {
+            _client = new HipchatClient(TestsConfig.AuthToken);
+        }
+
+        [Fact(DisplayName = "Can set the room topic of an existing room")]
+        public void CanSetRoomTopicOfExistingRoom()
+        {
+            var roomId = TestHelpers.GetARoomId((HipchatClient)_client, "Test Set Topic Room");
+            var result = _client.SetTopic(roomId, "This is the room's topic");
+            Assert.True(result);
+        }
+
+    }
+}
